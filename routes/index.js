ccd85aaec21a52ed6b5172d7f249878ac350ea3e
saranshj44 <saranshj44@gmail.com>
2021-01-04 14:22:13 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+router.get('/chat', function(req, res) {
+  res.render('chat');
+});
+module.exports = router;
